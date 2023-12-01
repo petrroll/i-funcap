ddc75150cc00ffc23222e07cc952bc135da41d3d
Petr Houška <houskape@gmail.com>
2023-12-01 19:00:51 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+## What is it?
+
+WIP of unofficial interactive version of [FUNCAP pre-print](https://www.preprints.org/manuscript/202309.2091/v1) questionnaire for assessing ME/CFS.
+
+## How does it work?
+
+`generate.py` generates individual variants based on `*.template.html` files and language specific questions in `/langs`. Currently very WIP, i.e. most questions are missing, there's no comparison with published cohorts, etc.
+
+Despite being WIP, any PRs welcome!
+
+## Plan (feel free to contribute before I add it)
+- finish UI, i.e. add answers properly, general description
+- rip all questions for english, and some other language variant
+- make it available on githubio domain or maybe under petrroll.cz somewhere?
+- add support for generating 55 and 27 questions versions
+- show where you fall in the populations reported in the paper
---
+++ generate.py
@@ -0,0 +1,51 @@
+import os
+
+langs_folder = "langs"
+
+def parse_language(file):
+    print("Processing language: " + file)
+
+    with open(f"{langs_folder}/{file}", 'r', encoding='utf-8') as f:
+        form = {}
+        current_section = None
+
+        for line in f:
+            if len(line) == 0: 
+                pass
+            elif line[0].isalpha():
+                current_section = []
+                form[line.strip()] = current_section
+            elif line[0].isnumeric():
+                current_section.append(line.strip())
+
+    return form
+
+def generate_language(form, lang):
+    with open("funcap.template.html", 'r', encoding='utf-8') as f:
+        funcap_template = f.read()
+    with open("question.template.html", 'r', encoding='utf-8') as f:
+        questions_template = f.read()
+    with open("section.template.html", 'r', encoding='utf-8') as f:
+        sections_template = f.read()
+
+    funcap_html = funcap_template
+    funcap_html = funcap_html.replace("<!-- LANGUAGE -->", lang)
+    for section, questions in form.items():
+        section_html = sections_template
+        section_html = section_html.replace("<!-- SECTION TEXT -->", section)
+        section_html = section_html.replace("SECTION ID", f"section{section[0]}")
+        for i, question in enumerate(questions):
+            questions_html = questions_template
+            questions_html = questions_html.replace("<!-- QUESTION TEXT -->", question)
+            questions_html = questions_html.replace("SECTION NAME QUESTION NAME", f"section{section[0]}_question{i}")
+            section_html = section_html.replace("<!-- QUESTIONS -->", questions_html)
+        funcap_html = funcap_html.replace("<!-- SECTIONS -->", section_html)
+    
+    open(f"funcap.{lang}.html", 'w', encoding='utf-8').write(funcap_html)
+    
+
+for file in os.listdir(langs_folder):
+    lang = file.split('_')[1].split('.')[0]
+    parsed_form = parse_language(file)
+    generate_language(parsed_form, lang)
+
